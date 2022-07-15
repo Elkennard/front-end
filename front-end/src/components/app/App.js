9ac07e5ca2666e4aa838c338a908eda222d10933
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-07-15 09:46:15 +0100

    initial setup of react

---
+++ front-end/src/components/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Cards from './cards/cards';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Shopping App</h1>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/app/cards/cards.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react"
+
+export default function Cards() {
+  const [products, setProducts] = useState([])
+
+  
+    
+    return (
+        <>
+          <h3>card 1</h3>  
+        </>
+        
+    )
+}
